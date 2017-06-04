a2e28f96c447e78864d7c41ccb5fe587c34700b3
Awlex <wieseralex580@gmail.com>
2017-06-04 16:05:34 +0200

    Preparation for submitting to PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'osu-parser',
+  packages = ['osu-parser'],
+  version = '0.1',
+  description = 'A library to compute .osu-files',
+  author = 'Alex Wieser',
+  author_email = 'wieseralex580@gmail.com',
+  url = 'https://github.com/Awlexus/python-osu-parser',
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', #TODO replace with actual link
+  keywords = ['osu', 'parse', 'beatmap'],
+  classifiers = [],
+)
